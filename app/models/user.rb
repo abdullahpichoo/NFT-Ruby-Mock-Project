a5f3bd5f29c2f0c011731401dbd49ae3ee17293c
abdullahpichoo <abdullah.pichoo@gmail.com>
2023-06-09 13:57:07 +0500

    fixed some styling issues, username length etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_one :wallet, dependent: :destroy
   has_one :cart, dependent: :destroy
 
+  validates :username, length: { maximum: 20 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
